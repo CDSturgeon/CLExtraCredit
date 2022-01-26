9ca586aea9866aa8e14ea17d9c048db15b107315
CDSturgeon <CDSturgeon@protonmail.com>
2022-01-26 15:38:16 -0500

    added class and struct to show both in one run

--- Class-v-Struct/Program.cs
+++ Class-v-Struct/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 namespace ConsoleApp1
 {
-    public class Person
+    public class PersonC
+    {
+        public byte Age { get; set; }
+    }
+    public struct PersonS
     {
         public byte Age { get; set; }
     }
@@ -9,14 +13,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Person p1 = new Person();
-            p1.Age = 21;
-            Console.WriteLine($"p1.Age = {p1.Age}");
-            Person p1Copy = p1;
-            p1Copy.Age = 55;
-            Console.WriteLine($"p1Copy.Age = {p1Copy.Age}");
+            //class reference type example
+            Console.WriteLine("Class object - reference type");
+            PersonC c1 = new PersonC(); //object from class
+            c1.Age = 21;
+            Console.WriteLine($"c1.Age = {c1.Age}");
+            PersonC c1Copy = c1; //reference type, points to same location in heap
+            c1Copy.Age = 55; //reference type changes object in heap, c1 and c1copy both .Age=55
+            Console.WriteLine($"c1Copy.Age = {c1Copy.Age}");
             // Does this change if Person is a class instead of a struct
-            Console.WriteLine($"p1.Age = {p1.Age}");
+            Console.WriteLine($"c1.Age = {c1.Age}\n\n");
+
+
+            //struct value type example
+            Console.WriteLine("Struct object - value type");
+            PersonS s1 = new PersonS();
+            s1.Age = 34;
+            Console.WriteLine($"s1.Age = {s1.Age}");
+            PersonS s1Copy = s1; //value type stays in stack. each has it's own value
+            s1Copy.Age = 99; //value type, did not change s1
+            Console.WriteLine($"s1Copy.Age = {s1Copy.Age}");
+            Console.WriteLine($"s1.Age = {s1.Age}");
         }
     }
 }
